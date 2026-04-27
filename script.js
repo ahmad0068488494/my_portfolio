70b12fbf7b59b708176d7cadfa09262c8cb9667d
ahmad0068488494 <ahmadalam0110@gmail.com>
2026-04-27 20:32:43 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-alert{
-    alert("Welcome to my Portfolio");
+function toggleDropdown() {
+  document.getElementById("dropdownMenu").classList.toggle("show");
 }
+
+// Close dropdown when clicking outside
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown-btn')) {
+    let dropdown = document.getElementById("dropdownMenu");
+    if (dropdown.classList.contains('show')) {
+      dropdown.classList.remove('show');
+    }
+  }
+};
